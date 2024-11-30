42b64f7624c4aea120f8dd43ece107577152988e
biubiu0628 <pewpew0628@gmail.com>
2024-11-30 10:42:45 +0700

    Add project files.

---
+++ baitap4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace baitap4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
